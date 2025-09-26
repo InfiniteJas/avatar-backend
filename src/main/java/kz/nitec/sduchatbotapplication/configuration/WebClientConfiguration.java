5ece12b967c266ee1d28f530870973f41f7e7029
Aisinus <xxxAuronblade277xxx@gmail.com>
2025-09-26 19:08:30 +0500

    empty commit по идее

--- src/main/java/kz/nitec/sduchatbotapplication/configuration/WebClientConfiguration.java
+++ src/main/java/kz/nitec/sduchatbotapplication/configuration/WebClientConfiguration.java
@@ -12,9 +12,7 @@ import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
 
-import java.time.Duration;
 
 @Configuration
 public class WebClientConfiguration {
